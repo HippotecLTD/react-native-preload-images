7097f444504936b85ae8cfda3c75f8f41ce10661
Matan Levi <matylevi3@gmail.com>
2019-09-05 14:20:17 +0300

    ADDED: Image Background Support

--- src/PreloadedImage.js
+++ src/PreloadedImage.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { ImageBackground } from 'react-native';
 import RNFetchBlob from 'react-native-fetch-blob';
 
 export default class extends React.Component {
   render () {
     return (
-      <Image
+      <ImageBackground
         {...this.props}
         source={{
           isStatic: true,
